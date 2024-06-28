7abd7bffe6ea93c6e09b52f560fc1a08f274d773
codingbear314 <wodnr2203@naver.com>
2024-06-28 22:20:38 +0900

    Added collision between particles

--- FluidByJS314.py
+++ FluidByJS314.py
@@ -3,12 +3,12 @@ import random
 
 BOX_SIZE = 3
 PARTICLE_RADIUS = 0.2
-DIPOLE_STRENGTH = 5
-LONDON_STRENGTH = 2
+DIPOLE_STRENGTH = 0.05
+LONDON_STRENGTH = 0.02
 FORCE_RANGE = 1
 PARTICLE_MASS = 1
 BOUNCE_FACTOR = 0.2
-COLLISION_FACTOR = 1
+COLLISION_FACTOR = 0.2
 
 class Particle:
     def __init__(self, pos, radius, color):
@@ -22,21 +22,23 @@ class Fluid:
     def __init__(self, number_of_particles):
         self.particles = []
         self.box = box(pos=vector(0, 0, 0), size=vector(BOX_SIZE, BOX_SIZE, BOX_SIZE), opacity=0.2)
-        for _ in range(number_of_particles):
-            x = random.uniform(-BOX_SIZE/2, BOX_SIZE/2)
-            y = random.uniform(-BOX_SIZE/2, BOX_SIZE/2)
-            z = random.uniform(-BOX_SIZE/2, BOX_SIZE/2)
-            particle = Particle(vector(x, y, z), PARTICLE_RADIUS, color.blue)
-            self.particles.append(particle)
+        for x in range(-12, 12, 4):
+            for y in range(-12, 12, 4):
+                for z in range(-12, 12, 4):
+                    xRandomness = random.uniform(-0.01, 0.01)
+                    yRandomness = random.uniform(-0.01, 0.01)
+                    zRandomness = random.uniform(-0.01, 0.01)
+                    particle = Particle(vector(x/10+xRandomness, y/10+yRandomness, z/10+zRandomness), PARTICLE_RADIUS, color.blue)
+                    self.particles.append(particle)
     def apply_interaction(self):
         for firstObj in range(len(self.particles)-1):
             for secondObj in range(firstObj+1, len(self.particles)):
                 r = self.particles[firstObj].obj.pos - self.particles[secondObj].obj.pos
                 distance = mag(r)
-                if distance < 0.2:
-                    distance = 0.2
+                if distance < 2*PARTICLE_RADIUS:
+                    distance = float('inf')
                 dpl_dpl_frc = -r * DIPOLE_STRENGTH / distance**3
-                ldn_frc = -r / distance**6
+                ldn_frc = -r * LONDON_STRENGTH / distance**6
                 self.particles[firstObj].force += dpl_dpl_frc + ldn_frc
                 self.particles[secondObj].force -= dpl_dpl_frc + ldn_frc
     def check_collision(self):
@@ -74,7 +76,6 @@ class Fluid:
         for particle in self.particles:
             # F = ma, a = F/m
             particle.acceleration = particle.force / PARTICLE_MASS
-            particle.velocity *= particle.velocityBounceFactor
             particle.velocity += particle.acceleration * dt
         
         # Check for collisions with other particles
@@ -103,12 +104,13 @@ class Fluid:
                 particle.obj.pos.z -= 2 * (particle.obj.pos.z + (BOX_SIZE/2 - PARTICLE_RADIUS))
 
         # Update position
-        for particle in self.particles:
+        for particle_idx in range(len(self.particles)):
+            particle = self.particles[particle_idx]
             particle.obj.pos += particle.velocity * dt
 
 def main():
-    fluid = Fluid(3)
-    dt = 0.01
+    fluid = Fluid(200)
+    dt = 0.005
     while True:
         rate(200)
         #print(fluid.particles[0].obj.pos, '\t', fluid.particles[0].velocity, '\t', fluid.particles[0].force)
